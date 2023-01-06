557ead02cf5231b5bbea79675eea7dde2bf15423
kanteumaxime <anagamax@gmail.com>
2023-01-06 06:33:36 +0100

    procedure pour ajouter un besoin

--- FEATURES/besoin/New.sql
+++ FEATURES/besoin/New.sql
@@ -1,7 +1,15 @@
+PROMPT ajouter un besoin
 SET DEFINE ON
+DECLARE
+v_paiement        varchar(15)           := &mode_paiement(mois,jour,ans);
+v_localisation    varchar(20)           := &votre_ville;
+n_user_id         integer               := (select user_id from utilisateur where email = '&Votre_adresse_mail');
+n_secteur_id      integer               := UPPER(SUBSTR('&secteur',1,5))
 Begin
-INSERT INTO BESOINS(besoin_id,paiement_par,localisation,user_id,secteur_id) VALUES (besion_sequence.NEXTVAL,'&paiement_par' ,'&Localisation',(select user_id from utilisateur where email = '&Votre_adresse_mail'),UPPER(SUBSTR('&secteur',1,5)));
+INSERT INTO 
+     BESOINS(besoin_id,paiement_par,localisation,user_id,secteur_id) 
+VALUES
+     (besion_sequence.NEXTVAL,'&mode_paiement(mois,jour,ans)' ,'&votre_ville',n_user_id ,n_secteur_id);
 end;
 /
-
 @@FEATURES/besoin/List
