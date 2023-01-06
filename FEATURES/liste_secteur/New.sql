c9384fa059d0a8fcadd3e15c1882313224785ab0
Malonju <malonjulaure@gmail.com>
2023-01-06 22:58:54 +0100

    creation des fonctionaliter de la table besoin

--- FEATURES/liste_secteur/New.sql
+++ FEATURES/liste_secteur/New.sql
@@ -1,8 +1,19 @@
-PROMPT inserting table liste_Secteur
-SET DEFINE OFF
-BEGIN
-INSERT INTO liste_secteurs(liste_secteur_id, secteur_id, user_id, profession) VALUES(liste_secteur_sequence.NEXTVAL, UPPER(SUBSTR('&secteur',1,5)), (select user_id where email = '&Votre_adresse_mail'), '&Votre_proffession');
-end;
-/
+declare
+    v_paiement_par VARCHAR (10 char) := '&paiement_par';
+    v_localisation VARCHAR (15 char)   := '&votre_localisation';
+    v_user_id int;
+    v_secteur_id varchar(5) := '&secteur_id' ;
+    v_secteur_nom varchar(10) := '&nom_secteur';
+    BEGIN
+     select user_id into v_user_id
+     from utilisateurs
+     where email = '&Votre_adresse_mail';
+     
+     INSERT INTO BESOINS(besoin_id,paiement_par,localisation,user_id,secteur_id) 
+     VALUES (besion_sequence.NEXTVAL,v_paiement_par,v_localisation, v_user_id,v_secteur_id);--1
+
+
+    END 
+    /
 
 @@FEATURES/liste_secteur/List
