904d9c92de8c2dfa962e6146cebe1ab9fb9eac35
azeukeng-brondon <brondongines@gmail.com>
2023-02-09 10:04:37 -0800

    Modification du fichier compte

---
+++ PLSQL/logout.sql
@@ -0,0 +1,34 @@
+clear screen
+
+PROMPT '                                  ---------------------------------------------------------------------------------------------------------                         '                                                                                                                           
+PROMPT '                                  |                                                                                                       |                         ' 
+PROMPT '                                  |                                                                                                       |                         '
+PROMPT '                                  |                                        Voulez vous vous deconnecter? :                                |                         '
+PROMPT '                                  |                   ********************************************************************                |                         '     
+prompt '                                  |                   *                                                                  *                |                         '
+PROMPT '                                  |                   *                                                                  *                |                         '
+prompt '                                  |                   *                    1.Oui                                         *                |                         ' 
+prompt '                                  |                   *                    2.Non                                         *                |                         '
+prompt '                                  |                   *                                                                  *                |                         '
+Prompt '                                  |                   *                                                                  *                |                         '
+PROMPT '                                  |                   ********************************************************************                |                         '
+PROMPT '                                  |                                                                                                       |                         '
+PROMPT '                                  |                                                                                                       |                         '
+PROMPT '                                  ---------------------------------------------------------------------------------------------------------                         '
+
+
+set term off
+set define ON
+
+column script new_value v_script
+
+select case '&selection.'
+       when '1' then '@PLSQL/start.sql'
+       when '2' then '@PLSQL/Compte/home.sql'
+       else 'menu'
+       end as script
+from dual;
+
+set term on
+
+@&v_script
